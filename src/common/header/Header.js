241a38978430523c3d39066ff62703dc05994c57
devatearth <64774809+devatearth@users.noreply.github.com>
2021-03-03 11:15:21 +0530

    Implemented Header and login form functionlity

---
+++ src/common/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../header/Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+        <header className='app-header'>
+            <div>Image Viewer</div>
+        </header>
+    
+    );
+  }
+}
+export default Header;

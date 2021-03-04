4058f10d69a8dee8ff4efc3bdf7278cebac1847d
devatearth <64774809+devatearth@users.noreply.github.com>
2021-03-04 09:28:27 +0530

    Implemented homepage Instagram API fetch

--- src/screen/home/Home.js
+++ src/screen/home/Home.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import "./Home.css";
 import Header from "../../common/header/Header";
-// import GridList from "@material-ui/core/GridList";
-// import GridListTile from "@material-ui/core/GridListTile";
-// import GridListTileBar from "@material-ui/core/GridListTileBar";
-
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 class Home extends Component {
-    
   constructor() {
     super();
     this.state = {
-      posts: {},
+      posts: [],
     };
   }
 
@@ -30,21 +36,25 @@ class Home extends Component {
     xhr.send(data);
     xhr.addEventListener("readystatechange", function() {
       if (this.readyState === 4) {
-        var response = JSON.parse(this.responseText);
-        var number_of_posts = response.data.length;
-        for (var i = 0; i < number_of_posts; i++) {
-        var id = response.data[i].id;
-        that.temp(id);
+        var response_Received = JSON.parse(this.responseText);
+        let number_of_posts = response_Received.data.length;
+        console.log("deva");
+        for (let i = 0; i < number_of_posts; i++) {
+          var id = response_Received.data[i].id;
+          that.temp(id);
         }
       }
     });
   }
-  temp = (id)=>{
+  temp = (id) => {
     let data = null;
     let xhr = new XMLHttpRequest();
+    let that = this;
     xhr.open(
       "GET",
-      "https://graph.instagram.com/"+id+"?fields=id,media_type,media_url,username,timestamp&access_token=" +
+      "https://graph.instagram.com/" +
+        id +
+        "?fields=id,media_type,media_url,username,timestamp,caption&access_token=" +
         sessionStorage.getItem("access-token")
     );
     xhr.setRequestHeader("Cache-Control", "no-cache");
@@ -52,39 +62,45 @@ class Home extends Component {
     xhr.addEventListener("readystatechange", function() {
       if (this.readyState === 4) {
         var response = JSON.parse(this.responseText);
-        for (var i = 0; i < response.data.length; i++) {
-        console.log(response.data[i]);
-        }
+        var posts_available = that.state.posts;
+        posts_available.push(response);
+        that.setState({
+          posts: posts_available,
+        });
       }
     });
-  } 
+  };
   render() {
     console.log(this.state);
     return (
       <div>
         <Header searchBox={true} profile={true} />
+
         <div className="flex-container">
-          {/* <GridList cellHeight={350} cols={2}>
+          <GridList cellHeight={600} cols={2} className="poster">
             {this.state.posts.map((post) => (
-            
-              <GridListTile key={"grid" + posts.id}>
-                <img
-                  src={movie.poster_url}
-                  className="movie-poster"
-                  alt={movie.title}
-                />
-                <GridListTileBar
-                  title={movie.title}
-                  subtitle={
-                    <span>
-                      Release Date:{" "}
-                      {new Date(movie.release_date).toDateString()}
-                    </span>
-                  }
-                />
-              </GridListTile>
+              <div>
+                <Card variant="outlined">
+                  <CardHeader
+                    avatar={<Avatar aria-label="recipe">R</Avatar>}
+                    action={
+                      <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                      </IconButton>
+                    }
+                    title={post.username}
+                    subheader={post.timestamp}
+                  />
+                  <CardContent>
+                    <GridListTile key={"grid" + post.id}>
+                      <img src={post.media_url} alt={post.caption} />
+                    </GridListTile>
+                  </CardContent>
+                </Card>
+                <br/>
+              </div>
             ))}
-          </GridList> */}
+          </GridList>
         </div>
       </div>
     );

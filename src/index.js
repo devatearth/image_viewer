1c0a9527b7862d6ea94e64e6309077ac0f1ef021
DEV <64774809+devatearth@users.noreply.github.com>
2021-03-15 21:52:52 +0530

    Update index.js
    Refactoring code comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Controller from './screen/Controller';
-
+/* Root element */
 ReactDOM.render(
     <span>
         <Controller/>
